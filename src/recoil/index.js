a7317105770c548086ff8cebd802a34e9a22d4bf
Echogithu <1391531024@qq.com>
2022-01-09 22:15:03 +0800

    ✨ feat: recoil select

--- src/recoil/index.js
+++ src/recoil/index.js
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const todoListState = atom({
   key: "todoListState",
@@ -9,3 +9,38 @@ export const todoListFilterState = atom({
   key: 'todoListFilterState',
   default: 'Show All',
 });
+
+export const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+
+    switch (filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isCompleted);
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isCompleted);
+      default:
+        return list
+    }
+  }
+})
+
+export const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isCompleted).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum * 100;
+
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted
+    }
+  }
+})
